a59bfe9226775947b4c8cc9e7e1911ed7fc1bb93
Alex S <Alexander2.Silvester@live.uwe.ac.uk>
2025-10-19 16:14:52 +0100

    First working prototype
    AR working on mobile
    Cube shows up and responds to clicks
    UI opens and responds to button presses

--- Project/Assets/clickScript.cs
+++ Project/Assets/clickScript.cs
@@ -1,11 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class clickScript : MonoBehaviour
 {
+    [SerializeField]
+    Canvas cube_UI;
+
+    [SerializeField]
+    RawImage background;
+
+    [SerializeField]
+    Color correct_color;
+
+    [SerializeField]
+    Color incorrect_color;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        cube_UI.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -14,8 +27,23 @@ public class clickScript : MonoBehaviour
         
     }
 
-    public void click()
+    public void show()
     {
-        Debug.Log("click");
+        cube_UI.gameObject.SetActive(true);
+    }
+
+    public void hide()
+    {
+        cube_UI.gameObject.SetActive(false);
+    }
+
+    public void answer(bool correct)
+    {
+        if(correct)
+        {
+            background.color = correct_color;
+            return;
+        }
+        background.color = incorrect_color;
     }
 }
